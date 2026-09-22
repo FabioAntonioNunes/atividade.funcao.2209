01a169df492435e82c691039beb124c60859bb9d
Fábio Antônio Nunes <f7653531@gmail.com>
2026-09-22 16:43:21 -0300

    Add files via upload

---
+++ atividade2209funcao/jss.js/ativ3.js
@@ -0,0 +1,6 @@
+/*Crie uma função chamada celsiusParaFahrenheit que receba uma temperatura
+em Celsius e retorne a conversão para Fahrenheit (F = (C × 1.8) + 32).
+ */
+function celsiusParaFahrenheit(temperatura){
+    
+}
