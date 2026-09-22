6d5fa2595d8ad83e0cfaec93889c9aa15ebf11c5
Fábio Antônio Nunes <f7653531@gmail.com>
2026-09-22 17:36:10 -0300

    Add files via upload

---
+++ atividade2209funcao/jss.js/ativ5.js
@@ -0,0 +1,17 @@
+/*Crie uma função chamada somarElementos que receba um array de números
+como parâmetro, percorra o vetor, some todos os valores e retorne o total. */
+function somarElementos(numeros[]){
+    let soma = 0
+    for(let numero of numeros[]){
+        soma = soma + numero;
+    }
+    return soma
+}
+function resolucao(){
+    let numeros[] 
+    while(limite === 1)
+    limite = Number(prompt("Digite 0 quando os numeros acabarem e 1 caso tenha mais numeros."))
+    if(limite === 1){
+    numeros[a] = prompt("Digite o número")
+    }
+}
